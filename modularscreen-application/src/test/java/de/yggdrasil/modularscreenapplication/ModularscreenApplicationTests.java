ad38350320750b603d16ea60d85cfeead1944b3d
Max <max@localhost>
2021-11-26 17:29:16 +0100

    initial commit

---
+++ modularscreen-application/src/test/java/de/yggdrasil/modularscreenapplication/ModularscreenApplicationTests.java
@@ -0,0 +1,13 @@
+package de.yggdrasil.modularscreenapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModularscreenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
